e447ecbd1782c276462b9f139152924cbb660a91
Avadhut <mali.avadhut7@gmail.com>
2025-01-03 22:09:08 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@react-three/drei'],
+  resolve: {
+    alias: {
+      '@react-three/fiber': 'node_modules/@react-three/fiber/dist/umd/react-three-fiber.js',
+      '@react-three/drei': 'node_modules/@react-three/drei/dist/umd/react-three-drei.js',
     },
   },
 });
