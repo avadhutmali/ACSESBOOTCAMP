ed9aa5977930fa89c652c6923b98f8bd5f39bdfa
yash_savalkar <yashsavalkar321@gmail.com>
2025-01-06 18:39:27 +0530

    sessionInfo

--- src/components/Flipcard.jsx
+++ src/components/Flipcard.jsx
@@ -4,9 +4,14 @@ import session1 from "./images/session1.png";
 import session2 from "./images/session2.png";
 import session3 from "./images/session3.png";
 import session4 from "./images/session4.png";
-
+import Session1 from "./images/session1Info.png";
+import Session2 from "./images/session2Info.png";
+import Session3 from "./images/session3Info.png";
+import Session4 from "./images/session4Info.png";
+ 
 const FlipCard = ({ EventName, des, index }) => {
   const images = [session1, session2, session3, session4];
+  const sessionImages =[Session1, Session2 ,Session3 , Session4];
   const [flipped, setFlipped] = useState(false);
 
   const handleClick = () => {
@@ -36,12 +41,17 @@ const FlipCard = ({ EventName, des, index }) => {
 
           {/* Back */}
           <div className="flip-card-back absolute w-full h-full bg-blue-500 text-white flex justify-center items-center rounded-2xl transform rotate-y-180">
-            <div className="text-center">
-              <h2 className="text-2xl font-bold">{EventName}</h2>
+            <div className="text-center p-2">
+              {/* <h2 className="text-2xl font-bold">{EventName}</h2>
               <p className="mt-2">{des}</p> {/* Display description */}
-              <button className="mt-4 bg-white text-blue-500 px-4 py-2 rounded-full">
+              {/* <button className="mt-4 bg-white text-blue-500 px-4 py-2 rounded-full">
                 Click Me
-              </button>
+              </button> */}
+               <img
+                src={sessionImages[index - 1]} // Access images using index-1
+                alt={`Event ${index - 1}`}
+                className="w-full h-full object-cover"
+              />
             </div>
           </div>
         </div>
